8c5fecc95878996eaf15de869d2abc1451e37d92
bp <bartek.paraf@gmail.com>
2023-01-13 21:05:40 +0100

    SuppliersController with index function and search/filter for name and email functionality added

---
+++ app/Http/Controllers/SuppliersController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Supplier;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class SuppliersController extends Controller
+{
+    function index(Request $request) {
+        return Inertia::render('Suppliers/Suppliers', [
+            'suppliers' => Supplier::when($request->search, function ($query, $search) {
+                $query->where('name', 'LIKE', '%'.$search.'%')
+                      ->orWhere('email', 'LIKE', '%'.$search.'%');
+            })->paginate(15)
+              ->withQueryString()
+        ]);
+    }
+}
