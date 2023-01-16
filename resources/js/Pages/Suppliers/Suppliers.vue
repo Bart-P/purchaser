9ddfc72508bc68df858173857dc213eb641b271d
bp <bartek.paraf@gmail.com>
2023-01-16 20:06:07 +0100

    changed style of suppliers main page so padding is same as at suppliers.create

--- resources/js/Pages/Suppliers/Suppliers.vue
+++ resources/js/Pages/Suppliers/Suppliers.vue
@@ -6,9 +6,9 @@
             Alle Lieferanten
         </template>
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-7xl mx-auto px-6">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg py-4">
                         <SuppliersTableNav :suppliers="suppliers" />
                         
                         <SuppliersTable :suppliers="suppliers" />
