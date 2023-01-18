ac48a561b71b88a34f7ba70937751db227d80aa7
bp <bartek.paraf@gmail.com>
2023-01-18 21:05:30 +0100

    added flowbite package

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,31 +2,32 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-    ],
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.vue',
+    './node_modules/flowbite/**/*.js',
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-            colors: {
-                'purchaser-primary': {
-                    light: '#f5dfef',
-                    DEFAULT: '#9B287B',
-                    dark: '#421134'
-                },
-                'purchaser-secondary': '#F96E46',
-                'purchaser-dark': '#00072D',
-                'purchaser-light': '#EFEFEF'
-            },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+      },
+      colors    : {
+        'purchaser-primary'  : {
+          light  : '#f5dfef',
+          DEFAULT: '#9B287B',
+          dark   : '#421134'
         },
+        'purchaser-secondary': '#F96E46',
+        'purchaser-dark'     : '#00072D',
+        'purchaser-light'    : '#EFEFEF'
+      },
     },
+  },
 
 
-    plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms'), require('flowbite/plugin')],
 };
