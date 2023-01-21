c0a41ad023ba77439b475dec43a347e468855a30
bp <bartek.paraf@gmail.com>
2023-01-21 19:09:23 +0100

    moved the single address card to its own component

---
+++ resources/js/Components/AddressInfoCard.vue
@@ -0,0 +1,55 @@
+<template>
+    <dl :id="id"
+        class="max-w-md text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700">
+        <div class="flex flex-col pb-3">
+            <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">Addresstyp</dt>
+            <dd class="text-lg font-semibold">{{ address.type }}</dd>
+        </div>
+        <div class="flex flex-col py-3">
+            <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">Name 1</dt>
+            <dd class="text-lg font-semibold">{{ address.name1 }}</dd>
+        </div>
+        <div v-if="address.name2"
+             class="flex flex-col py-3">
+            <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">Name 2</dt>
+            <dd class="text-lg font-semibold">{{ address.name2 }}</dd>
+        </div>
+        <div v-if="address.name3"
+             class="flex flex-col py-3">
+            <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">Name 3</dt>
+            <dd class="text-lg font-semibold">{{ address.name3 }}</dd>
+        </div>
+        <div class="flex flex-col py-3">
+            <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">Straße / Nr</dt>
+            <dd class="text-lg font-semibold">{{ address.street }} {{ address.streetNr }}</dd>
+        </div>
+        <div class="flex flex-col pt-3">
+            <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">PLZ / Ort</dt>
+            <dd class="text-lg font-semibold">{{ address.cityCode }} {{ address.city }}</dd>
+        </div>
+        <div class="flex flex-col pt-3">
+            <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">Land</dt>
+            <dd class="text-lg font-semibold">{{ address.country }}</dd>
+        </div>
+        <div v-if="address.phone"
+             class="flex flex-col py-3">
+            <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">Telefon</dt>
+            <dd class="text-lg font-semibold">{{ address.phone }}</dd>
+        </div>
+    </dl>
+</template>
+
+<script setup>
+
+defineProps(
+    {
+        address: {
+            default: null,
+            type   : Object,
+        },
+        id     : {
+            default: null,
+            type   : Number,
+        }
+    })
+</script>
--- resources/js/Pages/Suppliers/SuppliersCreate.vue
+++ resources/js/Pages/Suppliers/SuppliersCreate.vue
@@ -45,44 +45,9 @@
                                 @click="removeAddress(index)">
                             <i class="fa-solid fa-trash"></i>
                         </button>
-                        <dl :id="index"
-                            class="max-w-md text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700">
-                            <div class="flex flex-col pb-3">
-                                <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">Addresstyp</dt>
-                                <dd class="text-lg font-semibold">{{ address.type }}</dd>
-                            </div>
-                            <div class="flex flex-col py-3">
-                                <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">Name 1</dt>
-                                <dd class="text-lg font-semibold">{{ address.name1 }}</dd>
-                            </div>
-                            <div v-if="address.name2"
-                                 class="flex flex-col py-3">
-                                <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">Name 2</dt>
-                                <dd class="text-lg font-semibold">{{ address.name2 }}</dd>
-                            </div>
-                            <div v-if="address.name3"
-                                 class="flex flex-col py-3">
-                                <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">Name 3</dt>
-                                <dd class="text-lg font-semibold">{{ address.name3 }}</dd>
-                            </div>
-                            <div class="flex flex-col py-3">
-                                <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">Straße / Nr</dt>
-                                <dd class="text-lg font-semibold">{{ address.street }} {{ address.streetNr }}</dd>
-                            </div>
-                            <div class="flex flex-col pt-3">
-                                <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">PLZ / Ort</dt>
-                                <dd class="text-lg font-semibold">{{ address.cityCode }} {{ address.city }}</dd>
-                            </div>
-                            <div class="flex flex-col pt-3">
-                                <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">Land</dt>
-                                <dd class="text-lg font-semibold">{{ address.country }}</dd>
-                            </div>
-                            <div v-if="address.phone"
-                                 class="flex flex-col py-3">
-                                <dt class="mb-1 text-gray-500 text-sm dark:text-gray-400">Telefon</dt>
-                                <dd class="text-lg font-semibold">{{ address.phone }}</dd>
-                            </div>
-                        </dl>
+                        
+                        <AddressInfoCard :id="index"
+                                         :address="address" />
                     </div>
                 </div>
             </PageBoxWrapper>
@@ -252,6 +217,7 @@ import {initModals} from "flowbite";
 import PageBoxWrapper from "@/Components/PageBoxWrapper.vue";
 import AddSupplierForm from "@/Components/AddSupplierForm.vue";
 import AddAddressForm from "@/Components/AddAddressForm.vue";
+import AddressInfoCard from "@/Components/AddressInfoCard.vue";
 
 onMounted(() => {
     initModals()
