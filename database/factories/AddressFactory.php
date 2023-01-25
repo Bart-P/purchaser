331ac9441ca252ea3c47323eb0df7d549d9d2be8
bp <bartek.paraf@gmail.com>
2023-01-25 19:34:04 +0100

    set up address migration and factory

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -17,7 +17,34 @@ class AddressFactory extends Factory
     public function definition()
     {
         return [
-            // TODO
+            'supplier_id' => fake()->numberBetween(1, 100),
+            'type'        => fake()->randomElement(
+                [
+                    'main',
+                    'invoice',
+                    'delivery',
+                ]),
+            'name1'       => fake()->unique()->company(),
+            'name2'       => fake()->name(),
+            'name3'       => fake()->randomElement(
+                [
+                    '',
+                    'Hintereingang',
+                    '',
+                    'im Hof',
+                    '',
+                    'Bitte im Flur abstellen',
+                ]),
+            'street'      => fake()->streetAddress(),
+            'street_nr'   => fake()->numberBetween(1, 100),
+            'city_code'   => fake()->postcode(),
+            'city'        => fake()->city(),
+            'country'     => fake()->country(),
+            'phone'       => fake()->randomElement(
+                [
+                    fake()->phoneNumber(),
+                    null,
+                ]),
         ];
     }
 }
---
+++ database/migrations/2023_01_25_183434_create_addresses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->integer('supplier_id');
+            $table->string('type');
+            $table->string('name1')->unique();
+            $table->string('name2')->nullable();
+            $table->string('name3')->nullable();
+            $table->string('street');
+            $table->string('street_nr');
+            $table->string('city_code');
+            $table->string('city');
+            $table->string('country');
+            $table->string('phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
