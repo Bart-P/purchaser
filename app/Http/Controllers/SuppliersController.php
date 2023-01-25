83d91d74baf07162f4eb51f7edc57f743ec9dc49
bp <bartek.paraf@gmail.com>
2023-01-25 21:13:18 +0100

    SuppliersController can now create new addresses in db

--- app/Http/Controllers/SuppliersController.php
+++ app/Http/Controllers/SuppliersController.php
@@ -2,38 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
 use App\Models\Supplier;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class SuppliersController extends Controller
 {
-    function index(Request $request) {
-        return Inertia::render('Suppliers/Suppliers', [
-            'suppliers' => Supplier::when($request->search, function ($query, $search) {
-                $query->where('name', 'LIKE', '%'.$search.'%')
-                      ->orWhere('email', 'LIKE', '%'.$search.'%');
-            })->latest()
-              ->paginate(15)
-              ->withQueryString()
-        ]);
+    function index(Request $request)
+    {
+        return Inertia::render('Suppliers/Suppliers',
+                               [
+                                   'suppliers' => Supplier::when($request->search, function ($query, $search) {
+                                       $query->where('name', 'LIKE', '%' . $search . '%')
+                                             ->orWhere('email', 'LIKE', '%' . $search . '%');
+                                   })->latest()
+                                                          ->paginate(15)
+                                                          ->withQueryString(),
+                               ]);
     }
 
-    function create() {
+    function create()
+    {
         return Inertia::render('Suppliers/SuppliersCreate');
     }
 
-    function store(Request $request) {
-        $request->validate([
-             'name' => ['required', 'max:50'],
-             'email' => ['required', 'max:50'],
-             ]);
+    function store(Request $request)
+    {
+        $request->validate(
+            [
+                'name'  => ['required', 'max:50'],
+                'email' => ['required', 'max:50'],
+            ]);
 
-        Supplier::create([
-            'name' => $request->name,
-            'email' => $request->email,
-             ]);
+        $supplier = Supplier::create(
+            [
+                'name'  => $request->name,
+                'email' => $request->email,
+            ]);
 
+        if ($supplier) {
+            $addressesArray = [];
+            foreach ($request->addresses as $address) {
+                $address = new Address(
+                    [
+                        'supplier_id' => $supplier->id,
+                        'type'        => $address['type'],
+                        'name1'       => $address['name1'],
+                        'name2'       => $address['name2'],
+                        'name3'       => $address['name3'],
+                        'street'      => $address['street'],
+                        'street_nr'   => $address['streetNr'],
+                        'city_code'   => $address['cityCode'],
+                        'city'        => $address['cityCode'],
+                        'country'     => $address['cityCode'],
+                        'phone'       => $address['phone'],
+                    ]
+
+                );
+                $addressesArray[] = $address;
+            };
+            $this->storeAddressesAndOrPersons($supplier, $addressesArray);
+        }
+
+        // TODO set up error or success notification on route
         return redirect()->route('suppliers');
+    }
+
+    function storeAddressesAndOrPersons(Supplier $supplier, $addresses = [[]], $persons = [[]])
+    {
+        if (count($addresses)) {
+            $supplier->addresses()->saveMany($addresses);
+        }
     }
 }
