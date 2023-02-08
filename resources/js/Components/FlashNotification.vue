0b522472c544d40ce9e145074b1bf415d7f89f85
bp <bartek.paraf@gmail.com>
2023-02-08 20:31:39 +0100

    flash message resets now type in store to 'success'

--- resources/js/Components/FlashNotification.vue
+++ resources/js/Components/FlashNotification.vue
@@ -5,7 +5,7 @@
                  id="toast-success"
                  class="fixed top-[5rem] left-[50%] right-[50%] translate-x-[-50%] flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow-lg transition-opacity"
                  role="alert">
-                <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 bg-green-100 rounded-2xl">
+                <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-2xl bg-green-100">
                     <i class="fa-solid fa-check text-green-500"></i>
                     <span class="sr-only">Check icon</span>
                 </div>
@@ -22,8 +22,6 @@ import {store} from "@/store";
 import {watch} from "vue";
 import {usePage} from "@inertiajs/inertia-vue3";
 
-// Apply conditional classes,
-
 if (usePage().props.value.notification.message) {
     store.flash.message = usePage().props.value.notification.message
     setTimeout(() => {
@@ -35,6 +33,7 @@ watch(() => store.flash.message,
       () => {
           setTimeout(() => {
               store.flash.message = ''
+              store.flash.type = 'success'
           }, 5000)
       }
 )
