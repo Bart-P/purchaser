45edaac389c4155db64bda2f4e3a1aa54f727e53
bp <bartek.paraf@gmail.com>
2023-02-08 20:29:12 +0100

    changed HasMany to BelongsTo relationaship

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Address extends Model
 {
@@ -23,8 +23,8 @@ class Address extends Model
         'phone',
     ];
 
-    public function suppliers(): HasOne
+    public function suppliers(): BelongsTo
     {
-        return $this->hasOne(Supplier::class);
+        return $this->belongsTo(Supplier::class);
     }
 }
