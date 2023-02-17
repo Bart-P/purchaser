017f1cf57995bfffa3983df7569db19ee83ab8c9
bp <bartek.paraf@gmail.com>
2023-02-17 20:51:48 +0100

    name change to same as in db

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -22,32 +22,32 @@ class AddressController extends Controller
     {
         $request->validate(
             [
-                'supplierId' => 'required|integer',
-                'type'       => 'required|max:255',
-                'name1'      => 'required|max:255',
-                'name2'      => 'nullable|max:255',
-                'name3'      => 'nullable|max:255',
-                'street'     => 'required|max:255',
-                'streetNr'   => 'required|max:255',
-                'cityCode'   => 'required|max:255',
-                'city'       => 'required|max:255',
-                'country'    => 'required|max:255',
-                'phone'      => 'nullable|max:255',
+                'supplier_id' => 'required|integer',
+                'type'        => 'required|max:255',
+                'name1'       => 'required|max:255',
+                'name2'       => 'nullable|max:255',
+                'name3'       => 'nullable|max:255',
+                'street'      => 'required|max:255',
+                'street_nr'   => 'required|max:255',
+                'city_code'   => 'required|max:255',
+                'city'        => 'required|max:255',
+                'country'     => 'required|max:255',
+                'phone'       => 'nullable|max:255',
             ]
         );
 
-        Supplier::find($request->supplierId)->touch();
+        Supplier::find($request->supplier_id)->touch();
 
         Address::create(
             [
-                'supplier_id' => $request->supplierId,
+                'supplier_id' => $request->supplier_id,
                 'type'        => $request->type,
                 'name1'       => $request->name1,
                 'name2'       => $request->name2,
                 'name3'       => $request->name3,
                 'street'      => $request->street,
-                'street_nr'   => $request->streetNr,
-                'city_code'   => $request->cityCode,
+                'street_nr'   => $request->street_nr,
+                'city_code'   => $request->city_code,
                 'city'        => $request->city,
                 'country'     => $request->country,
                 'phone'       => $request->phone,
