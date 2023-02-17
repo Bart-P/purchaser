21ef24cdd1b8364cb6324a1697e3b863f2b42203
bp <bartek.paraf@gmail.com>
2023-02-17 20:53:39 +0100

    added link to edit page for each persond card

--- resources/js/Pages/Suppliers/SuppliersEdit.vue
+++ resources/js/Pages/Suppliers/SuppliersEdit.vue
@@ -122,11 +122,19 @@
                     <div class="flex flex-wrap gap-8 pt-8">
                         <template v-for="(person, index) in persons">
                             <div class="w-[31.5%] max-w-md min-w-[250px] p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                                <button class="float-right text-red-600"
-                                        type="submit"
-                                        @click="setPersonToDelete(person.first_name, person.last_name, person.id)">
-                                    <i class="fa-solid fa-trash"></i>
-                                </button>
+                                
+                                <div class="float-right flex gap-4">
+                                    <button class="float-right text-red-600"
+                                            type="submit"
+                                            @click="setPersonToDelete(person.first_name, person.last_name, person.id)">
+                                        <i class="fa-solid fa-trash"></i>
+                                    </button>
+                                    <Link v-if="person.id"
+                                          :href="route('person.edit', person.id)"
+                                          class="text-blue-600">
+                                        <i class="fa-solid fa-pen"></i>
+                                    </Link>
+                                </div>
                                 <SinglePersonList :id="index"
                                                   :person="person" />
                             </div>
