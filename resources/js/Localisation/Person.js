4bcb7b648ea560acc6f1933cd7a68b48e2939e74
bp <bartek.paraf@gmail.com>
2023-02-19 12:35:10 +0100

    persond translation of type and gender added

---
+++ resources/js/Localisation/Person.js
@@ -0,0 +1,17 @@
+export const Person = {
+  type  : {
+    de: {
+      main    : 'hauptkontakt',
+      secondar: 'vertretung',
+      other   : 'andere',
+    }
+  },
+  gender: {
+    de: {
+      m: 'männlich',
+      f: 'weiblich',
+      o: 'andere',
+    }
+  }
+
+}
