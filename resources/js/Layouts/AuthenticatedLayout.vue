da7d82c059c958af3980b55ca56144ef6c0e2f32
bp <bartek.paraf@gmail.com>
2023-02-25 21:02:08 +0100

    added new ToastList component which handles the notifications

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,16 +1,15 @@
 <script setup>
-import TopNavBar from "@/Components/TopNavBar.vue";
-
-</script>
+import ToastList from '@/Components/ToastList.vue';
+import TopNavBar from '@/Components/TopNavBar.vue';</script>
 
 <template>
     <div>
         <div class="min-h-screen bg-purchaser-light">
-            
+            <ToastList />
             <TopNavBar />
-
             <!-- Page Heading -->
-            <header class="bg-white shadow" v-if="$slots.header">
+            <header class="bg-white shadow"
+                    v-if="$slots.header">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     <h2 class="font-semibold text-xl text-purchaser-dark leading-tight">
                         <slot name="header" />
