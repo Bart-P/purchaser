26c011b3ab8e184fa02cf39d2a7d5b1efd2a51cf
bp <bartek.paraf@gmail.com>
2023-03-10 20:45:08 +0100

    addet transition styles for when categories are selected

--- resources/js/Pages/Suppliers/Partials/AddSupplierForm.vue
+++ resources/js/Pages/Suppliers/Partials/AddSupplierForm.vue
@@ -214,3 +214,16 @@ function submitSupplier() {
 
 }
 </script>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+    transition: opacity 0.2s ease-in-out;
+}
+
+.v-enter-from,
+.v-leave-to {
+    opacity: 0;
+}
+
+</style>
