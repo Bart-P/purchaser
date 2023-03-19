d102dcb009860b3d6701aa2f0908797988d68a1a
bp <bartek.paraf@gmail.com>
2023-03-19 18:09:10 +0100

    created save address, person and createCategoryJunctions so it all comes from the Supplier model

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Http\Controllers\AddressController;
+use App\Http\Controllers\PersonController;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -20,14 +22,42 @@ class Supplier extends Model
         return $this->hasMany(Address::class);
     }
 
+    public function saveAddresses($addresses)
+    {
+        if ($addresses) {
+            $addressesArray = AddressController::getArrayOfAddressObjects($addresses, $this->id);
+            $this->addresses()->saveMany($addressesArray);
+        };
+    }
+
     public function persons(): HasMany
     {
         return $this->hasMany(Person::class);
     }
 
+    public function savePersons($persons)
+    {
+        if ($persons) {
+            $personsArray = PersonController::getArrayOfPersonObjects($persons, $this->id);
+            $this->persons()->saveMany($personsArray);
+        }
+    }
+
     public function categoryJunctions(): HasMany
     {
         return $this->hasMany(SupplierCategoryJunction::class);
+    }
+
+    public function createCategoryJunctions($categories)
+    {
+        foreach ($categories as $category) {
+            SupplierCategoryJunction::create(
+                [
+                    'supplier_id' => $this->id,
+                    'category_id' => $category['id'],
+                ]
+            );
+        }
     }
 
     public function delete(): ?bool
