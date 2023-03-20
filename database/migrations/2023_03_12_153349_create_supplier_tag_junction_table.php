5dd5702eed8714e7f4fc0f8c4f3b54ba4d9743e0
bp <bartek.paraf@gmail.com>
2023-03-20 21:11:51 +0100

    created supplier tag junction migration

---
+++ database/migrations/2023_03_12_153349_create_supplier_tag_junction_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supplier_tag_junction', function (Blueprint $table) {
+            $table->id();
+            $table->integer('supplier_id');
+            $table->integer('tag_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('supplier_tag_junction');
+    }
+};
