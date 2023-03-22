39cf805f434f964fdf7ce1310fa848bb317a1e9a
bp <bartek.paraf@gmail.com>
2023-03-22 18:20:53 +0100

    reformated code

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -1,36 +1,18 @@
-<script setup>
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Link, useForm, usePage } from '@inertiajs/inertia-vue3';
-
-const props = defineProps({
-    mustVerifyEmail: Boolean,
-    status: String,
-});
-
-const user = usePage().props.value.auth.user;
-
-const form = useForm({
-    name: user.name,
-    email: user.email,
-});
-</script>
-
 <template>
     <section>
         <header>
-            <h2 class="text-lg font-medium text-gray-900">Profile Information</h2>
+            <h2 class="heading-2">Profile Information</h2>
 
             <p class="mt-1 text-sm text-gray-600">
                 Update your account's profile information and email address.
             </p>
         </header>
 
-        <form @submit.prevent="form.patch(route('profile.update'))" class="mt-6 space-y-6">
+        <form @submit.prevent="form.patch(route('profile.update'))"
+              class="mt-6 space-y-6">
             <div>
-                <InputLabel for="name" value="Name" />
+                <InputLabel for="name"
+                            value="Name" />
 
                 <TextInput
                     id="name"
@@ -42,11 +24,13 @@ const form = useForm({
                     autocomplete="name"
                 />
 
-                <InputError class="mt-2" :message="form.errors.name" />
+                <InputError class="mt-2"
+                            :message="form.errors.name" />
             </div>
 
             <div>
-                <InputLabel for="email" value="Email" />
+                <InputLabel for="email"
+                            value="Email" />
 
                 <TextInput
                     id="email"
@@ -57,7 +41,8 @@ const form = useForm({
                     autocomplete="email"
                 />
 
-                <InputError class="mt-2" :message="form.errors.email" />
+                <InputError class="mt-2"
+                            :message="form.errors.email" />
             </div>
 
             <div v-if="props.mustVerifyEmail && user.email_verified_at === null">
@@ -84,10 +69,35 @@ const form = useForm({
             <div class="flex items-center gap-4">
                 <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
 
-                <Transition enter-from-class="opacity-0" leave-to-class="opacity-0" class="transition ease-in-out">
-                    <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>
+                <Transition enter-from-class="opacity-0"
+                            leave-to-class="opacity-0"
+                            class="transition ease-in-out">
+                    <p v-if="form.recentlySuccessful"
+                       class="text-sm text-gray-600">Saved.</p>
                 </Transition>
             </div>
         </form>
     </section>
 </template>
+
+<script setup>
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import {Link, useForm, usePage} from '@inertiajs/inertia-vue3';
+
+const props = defineProps(
+    {
+        mustVerifyEmail: Boolean,
+        status         : String,
+    });
+
+const user = usePage().props.value.auth.user;
+
+const form = useForm(
+    {
+        name : user.name,
+        email: user.email,
+    });
+</script>
