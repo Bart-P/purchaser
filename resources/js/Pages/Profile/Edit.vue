053cfb70842516a019bbea0a35cbdc7aabca2931
bp <bartek.paraf@gmail.com>
2023-03-22 18:20:38 +0100

    addapted page layout and boxes so its conform with all other pages, used my reusable components instead of Breeze components

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -1,16 +1,3 @@
-<script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import DeleteUserForm from './Partials/DeleteUserForm.vue';
-import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
-import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
-import { Head } from '@inertiajs/inertia-vue3';
-
-defineProps({
-    mustVerifyEmail: Boolean,
-    status: String,
-});
-</script>
-
 <template>
     <Head title="Profile" />
 
@@ -20,23 +7,36 @@ defineProps({
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="max-w-7xl mx-auto space-y-6">
+                <PageBoxWrapper>
                     <UpdateProfileInformationForm
                         :must-verify-email="mustVerifyEmail"
                         :status="status"
                         class="max-w-xl"
                     />
-                </div>
-
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                </PageBoxWrapper>
+                <PageBoxWrapper>
                     <UpdatePasswordForm class="max-w-xl" />
-                </div>
+                </PageBoxWrapper>
 
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <PageBoxWrapper>
                     <DeleteUserForm class="max-w-xl" />
-                </div>
+                </PageBoxWrapper>
             </div>
         </div>
     </AuthenticatedLayout>
 </template>
+
+<script setup>
+import PageBoxWrapper from '@/Components/PageBoxWrapper.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import {Head} from '@inertiajs/inertia-vue3';
+import DeleteUserForm from './Partials/DeleteUserForm.vue';
+import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
+import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
+
+defineProps({
+                mustVerifyEmail: Boolean,
+                status         : String,
+            });
+</script>
