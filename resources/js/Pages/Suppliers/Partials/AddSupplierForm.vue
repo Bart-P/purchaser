e64576f06c18d2347c46c612e899cdc7b95f9129
bp <bartek.paraf@gmail.com>
2023-03-23 21:18:55 +0100

    added flowbite drawer for tag choice

--- resources/js/Pages/Suppliers/Partials/AddSupplierForm.vue
+++ resources/js/Pages/Suppliers/Partials/AddSupplierForm.vue
@@ -15,6 +15,53 @@
             </div>
         </div>
 
+        <!-- drawer init and show -->
+        <div class="text-center">
+
+        </div>
+
+        <!-- drawer component -->
+        <div id="choose-tag-drawer"
+             class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-80 dark:bg-gray-800"
+             tabindex="-1"
+             aria-labelledby="choose-tag-drawer-label">
+            <h5 id="choose-tag-drawer-label"
+                class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">Menu</h5>
+            <button type="button"
+                    data-drawer-hide="choose-tag-drawer"
+                    aria-controls="choose-tag-drawer"
+                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                <svg aria-hidden="true"
+                     class="w-5 h-5"
+                     fill="currentColor"
+                     viewBox="0 0 20 20"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                          clip-rule="evenodd"></path>
+                </svg>
+                <span class="sr-only">Close menu</span>
+            </button>
+            <div class="py-4 overflow-y-auto">
+                <ul class="space-y-2">
+                    <template v-for="category in checkedCategories">
+                        <li v-if="category.tags.length"
+                            class="border-t-2 border-purchaser-primary">
+                            <h4 class="heading-4">{{ category.name }}</h4>
+                            <ul class="space-y-2 py-2 mt-2">
+                                <li v-for="tag in category.tags"
+                                    :style="{backgroundColor: category.color}"
+                                    class="tag">{{ tag.name }}
+                                </li>
+                            </ul>
+                        </li>
+                    </template>
+
+                </ul>
+            </div>
+        </div>
+
+
         <div class="flex gap-5">
             <div class="w-1/2">
                 <InputLabel for="company"
@@ -95,16 +142,21 @@
         <div class="space-y-6">
             <div class="flex justify-between items-center">
                 <h4 class="heading-4">Tags (optional)</h4>
-                <BaseButton id="chooseTagsModal"
-                            class="inline-flex items-center"
-                            type="button">Tag Auswählen
+                <BaseButton
+                    color="primary"
+                    type="button"
+                    class="inline-flex items-center"
+                    data-drawer-target="choose-tag-drawer"
+                    data-drawer-show="choose-tag-drawer"
+                    aria-controls="choose-tag-drawer">
+                    Tag Auswählen
                 </BaseButton>
             </div>
 
             <ul class="flex flex-wrap items-center gap-4 uppercase">
                 <li v-for="tag in checkedTags"
                     :style="{backgroundColor: tag.color}"
-                    class="text-white font-bold text-sm font-medium mr-2 px-2.5 py-0.5 rounded">
+                    class="tag">
                     {{ tag.name }}
                 </li>
             </ul>
@@ -120,11 +172,12 @@ import SelectCategoryDropdown from '@/Components/SelectCategoryDropdown.vue';
 import TextInput from '@/Components/TextInput.vue';
 import Toast from '@/Stores/toast';
 import {useForm} from '@inertiajs/inertia-vue3';
-import {initDropdowns} from 'flowbite';
+import {initDrawers, initDropdowns} from 'flowbite';
 import {onMounted, ref} from 'vue';
 
 onMounted(() => {
     initDropdowns();
+    initDrawers();
 })
 
 const props = defineProps(
