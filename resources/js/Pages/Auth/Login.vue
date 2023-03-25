43ee3f12974bdd8685ef151e58e68402226f3b31
bp <bartek.paraf@gmail.com>
2023-03-25 11:56:23 +0100

    reformated and removed commented out code which is not used anymore

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,90 +1,50 @@
 <template>
     <GuestLayout>
+
         <Head title="Log in" />
 
-        <div v-if="status"
-             class="mb-4 font-medium text-sm text-green-600">
+        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
             {{ status }}
         </div>
 
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="email"
-                            value="Email" />
+                <InputLabel for="email" value="Email" />
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
+                <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus
+                    autocomplete="username" />
 
-                <InputError class="mt-2"
-                            :message="form.errors.email" />
+                <InputError class="mt-2" :message="form.errors.email" />
             </div>
 
             <div class="mt-4">
-                <InputLabel for="password"
-                            value="Password" />
+                <InputLabel for="password" value="Password" />
 
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
+                <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required
+                    autocomplete="current-password" />
 
-                <InputError class="mt-2"
-                            :message="form.errors.password" />
+                <InputError class="mt-2" :message="form.errors.password" />
             </div>
 
             <div class="block mt-4">
                 <label class="flex items-center">
-                    <Checkbox name="remember"
-                              v-model:checked="form.remember" />
+                    <Checkbox name="remember" v-model:checked="form.remember" />
                     <span class="ml-2 text-sm text-gray-600">Remember me!</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-between mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Forgot your password?
+                <Link v-if="canResetPassword" :href="route('password.request')"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Forgot your password?
                 </Link>
-                <Link
-                    :href="route('register')"
-                    class="
-                    underline
-                    text-sm
-                    text-gray-600
-                    hover:text-gray-900
-                    rounded-md
-                    focus:outline-none
-                    focus:ring-2
-                    focus:ring-offset-2
-                    focus:ring-indigo-500"
-                >
-                    No account yet?
+                <Link :href="route('register')"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                No account yet?
                 </Link>
 
-                <!--
-                                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                                    Log in
-                                </PrimaryButton>
-                -->
-
-                <BaseButton type="submit"
-                            :class="{ 'opacity-25': form.processing }"
-                            :disabled="form.processing"
-                            color="primary">Log In
+                <BaseButton type="submit" :class="{ 'opacity-25': form.processing }" :disabled="form.processing"
+                    color="primary">Log In
                 </BaseButton>
             </div>
         </form>
@@ -98,18 +58,18 @@ import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import TextInput from '@/Components/TextInput.vue';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
-import {Head, Link, useForm} from '@inertiajs/inertia-vue3';
+import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
 
 defineProps({
-                canResetPassword: Boolean,
-                status          : String,
-            });
+    canResetPassword: Boolean,
+    status: String,
+});
 
 const form = useForm({
-                         email   : '',
-                         password: '',
-                         remember: false,
-                     });
+    email: '',
+    password: '',
+    remember: false,
+});
 
 const submit = () => {
     form.post(route('login'), {
