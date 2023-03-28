0cbb3f31d7925cf1ccd18f25d775792a22f42291
bp <bartek.paraf@gmail.com>
2023-03-28 19:38:00 +0200

    tagseeder now handles that

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,36 +34,7 @@ class DatabaseSeeder extends Seeder
         Person::factory(125)->create();
 
         $this->call([CategorySeeder::class]);
-
-        $tags = [
-            'offset'            => 1,
-            'bis B1'            => 1,
-            'bis B0'            => 1,
-            'digital'           => 1,
-            'thekendisplay'     => 1,
-            'pulverbeschichten' => 2,
-            'lackieren'         => 2,
-            'draht'             => 2,
-            'blech'             => 2,
-            'leichtholz'        => 3,
-            'möbelbau'          => 3,
-            'boxen'             => 3,
-            'lasern'            => 3,
-            'drucken'           => 3,
-            'folie'             => 4,
-            'magnetfolie'       => 4,
-            'pvc'               => 4,
-        ];
-
-        $tag_keys = array_keys($tags);
-        foreach ($tag_keys as $tag_key) {
-            Tag::factory()->create(
-                [
-                    'name'        => $tag_key,
-                    'category_id' => $tags[$tag_key],
-                ]
-            );
-        };
+        $this->call([TagSeeder::class]);
 
         $supplier_ids = Supplier::all('id');
         $category_ids = Category::all('id');
