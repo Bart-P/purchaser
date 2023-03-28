6f9858e25e9534a5698ac88baa14dfb01991f3d7
bp <bartek.paraf@gmail.com>
2023-03-28 19:35:14 +0200

    separate Tag Seeder created

---
+++ database/seeders/TagSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = [
+            ['name' => 'offset', 'category_id' => 1],
+            ['name' => 'bis B1', 'category_id' => 1],
+            ['name' => 'bis B0', 'category_id' => 1],
+            ['name' => 'digital', 'category_id' => 1],
+            ['name' => 'thekendisplay', 'category_id' => 1],
+            ['name' => 'pulverbeschichten', 'category_id' => 2],
+            ['name' => 'lackieren', 'category_id' => 2],
+            ['name' => 'draht', 'category_id' => 2],
+            ['name' => 'blech', 'category_id' => 2],
+            ['name' => 'leichtholz', 'category_id' => 3],
+            ['name' => 'möbelbau', 'category_id' => 3],
+            ['name' => 'boxen', 'category_id' => 3],
+            ['name' => 'lasern', 'category_id' => 3],
+            ['name' => 'drucken', 'category_id' => 3],
+            ['name' => 'folie', 'category_id' => 4],
+            ['name' => 'magnetfolie', 'category_id' => 4],
+            ['name' => 'pvc', 'category_id' => 4],
+        ];
+
+        Tag::factory()->createMany($tags);
+    }
+}
