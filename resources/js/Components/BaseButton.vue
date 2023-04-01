651d60ca0b9dc8ee45246e371dbcd691990f904b
bp <bartek.paraf@gmail.com>
2023-04-01 18:38:59 +0200

    reformated

--- resources/js/Components/BaseButton.vue
+++ resources/js/Components/BaseButton.vue
@@ -1,37 +1,36 @@
 <template>
-    <component :is="this.$attrs.href ? Link : 'button'"
-               :class="this.$attrs.disabled ? buttonStyles.disabled : getClass()"
-               class="cursor-pointer px-6 py-2 rounded">
+    <component :is="this.$attrs.href ? Link : 'button'" :class="this.$attrs.disabled ? buttonStyles.disabled : getClass()"
+        class="cursor-pointer px-6 py-2 rounded">
         <slot />
     </component>
 </template>
 
 <script setup>
 
-import {Link} from "@inertiajs/inertia-vue3";
+import { Link } from "@inertiajs/inertia-vue3";
 
 const props = defineProps(
     {
         color: {
-            type   : String,
+            type: String,
             default: 'primary'
         },
     })
 
 const buttonStyles = {
-    primary  : {
+    primary: {
         'bg-purchaser-primary hover:bg-purchaser-primary-dark text-white': true
     },
     secondary: {
         'bg-purchaser-secondary hover:bg-purchaser-primary-dark text-white': true
     },
-    light    : {
+    light: {
         'bg-gray-100 hover:bg-purchaser-primary-light text-purchaser-primary-dark': true
     },
-    disabled : {
+    disabled: {
         'cursor-not-allowed bg-gray-100 text-gray-500 px-6 py-2 rounded': true
     },
-    danger   : {
+    danger: {
         'bg-red-600 hover:bg-red-700 text-white px-6 py-2 rounded': true
     },
 }
