df805597b08dbef6c6458e516f24b3148717d7d6
bp <bartek.paraf@gmail.com>
2023-04-04 19:16:05 +0200

    Category Controller created with store

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    function store(Request $request)
+    {
+        $category = Category::create(
+            $request->validate(
+                [
+                    'name'  => ['required', 'max:50'],
+                    'color' => ['required', 'max:50'],
+                ]
+            )
+        );
+
+        if ($category) {
+
+            return redirect()->back()->with('notification', [
+                'message' => 'Kategorie hinzugefügt!',
+                'type'    => 'success',
+            ]);
+        }
+        return redirect()->back()->with('notification', [
+            'message' => 'Es ist ein Fehler aufgetreten! Kategorie wurde nicht gespeichert.',
+            'type'    => 'danger',
+        ]);
+    }
+}
