507946ec8ac2141c9aec2b4bc2ae38baa8710643
bp <bartek.paraf@gmail.com>
2023-04-07 19:25:25 +0200

    category controlls moved to its own component

---
+++ resources/js/Pages/Suppliers/Partials/CategoryControlls.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="space-x-2">
+        <Transition>
+            <button v-show="selectedCategory.id" data-dropdown-toggle="dropdownEditCategory" id="toggleEditCategoryDropdown"
+                class="text-blue-500 px-1 py-1 rounded-md border-blue-500 hover:bg-blue-500 hover:text-white">
+                <i class="fa-solid fa-pen"></i>
+            </button>
+        </Transition>
+        <Transition>
+            <button v-show="selectedCategory.id" data-dropdown-toggle="dropdownDeleteCategory"
+                id="toggleDeleteCategoryDropdown"
+                class="text-red-500 px-1 py-1 rounded-md border-red-500 hover:bg-red-500 hover:text-white">
+                <i class="fa-solid fa-trash"></i>
+            </button>
+        </Transition>
+        <button data-dropdown-toggle="dropdownAddCategory" type="button" id="toggleAddCategoryDropdown"
+            class="text-green-500 px-1 py-1 rounded-md hover:bg-green-500 hover:text-white">
+            <i class="fa-solid fa-plus"></i>
+        </button>
+    </div>
+    <form id="dropdownAddCategory" @submit.prevent="addNewCategory"
+        class="!m-0 flex flex-col hidden z-10 gap-4 rounded-md bg-white shadow-md p-4">
+        <h5 class="heading-5">Kategorie hinzufügen</h5>
+        <TextInput type="text" placeholder="Name" v-model="addCategoryForm.name" required />
+        <TextInput type="text" placeholder="Farbe" v-model="addCategoryForm.color" required />
+        <button type="submit"
+            class="text-white px-2 py-1 w-full hover:text-green-500 hover:bg-white border-2 border-green-500 bg-green-500 rounded-md">
+            <i class="fa-solid fa-save"></i>
+        </button>
+    </form>
+    <form id="dropdownEditCategory" @submit.prevent="updateCategory"
+        class="!m-0 flex flex-col z-10 gap-4 rounded-md bg-white shadow-md p-4 hidden">
+        <h5 class="heading-5">Kategorie bearbeiten</h5>
+        <TextInput type="text" placeholder="Name" v-model="selectedCategory.name" />
+        <TextInput type="text" placeholder="Farbe" v-model="selectedCategory.color" />
+        <button type="submit"
+            class="text-white px-2 py-1 w-full hover:text-green-500 hover:bg-white border-2 border-green-500 bg-green-500 rounded-md">
+            <i class="fa-solid fa-save"></i>
+        </button>
+    </form>
+    <form id="dropdownDeleteCategory" @submit.prevent="deleteSelectedCategory"
+        class="!m-0 flex flex-col hidden z-10 gap-4 rounded-md bg-white shadow-md p-4">
+        <h5 class="heading-5">Kategorie Löschen</h5>
+        <div class="text-center">
+            <p>
+                Soll die Kategorie <span class="font-bold">"{{ selectedCategory.name }}"</span>
+            </p>
+            <p>
+                wirklich gelöscht werden?
+            </p>
+        </div>
+        <button type="submit"
+            class="text-white px-2 py-1 w-full hover:text-red-500 hover:bg-white border-2 border-red-500 bg-red-500 rounded-md">
+            <i class="fa-solid fa-trash"></i>
+        </button>
+    </form>
+</template>
+<script setup>
+
+import { useForm } from '@inertiajs/inertia-vue3'
+import TextInput from '@/Components/TextInput.vue'
+import { onMounted } from 'vue';
+import { initDropdowns } from 'flowbite'
+import { Inertia } from '@inertiajs/inertia'
+
+onMounted(() => {
+    initDropdowns()
+})
+
+const props = defineProps({
+    selectedCategory: Object
+});
+
+const emits = defineEmits(['resetSelectableValues']);
+
+const addCategoryForm = useForm(
+    {
+        name: null,
+        color: null,
+    },
+)
+
+function addNewCategory() {
+    if (addCategoryForm.name && addCategoryForm.color) {
+        addCategoryForm.post(route('category.store'))
+        document.getElementById('toggleAddCategoryDropdown').click()
+        addCategoryForm.reset()
+    }
+}
+
+function deleteSelectedCategory() {
+    const idToDelete = props.selectedCategory.id
+
+    if (idToDelete) {
+        emits('resetSelectableValues')
+        Inertia.delete(route('category.destroy', idToDelete))
+        document.getElementById('toggleDeleteCategoryDropdown').click()
+    }
+}
+
+function updateCategory() {
+    const selectedCat = props.selectedCategory
+    if (selectedCat.name && selectedCat.color) {
+        Inertia.patch(route('category.patch', selectedCat))
+        document.getElementById('toggleEditCategoryDropdown').click()
+    }
+}
+
+</script>
+
+<style scoped></style>
--- resources/js/Pages/Suppliers/Partials/EditCategoriesModal.vue
+++ resources/js/Pages/Suppliers/Partials/EditCategoriesModal.vue
@@ -4,62 +4,10 @@
             <div class="basis-1/3 space-y-2">
                 <div class="flex items-center justify-between gap-2">
                     <h4 class="heading-4">Kategorien</h4>
-                    <div class="space-x-2">
-                        <Transition>
-                            <button v-show="selectedCategory.id" data-dropdown-toggle="dropdownEditCategory"
-                                id="toggleEditCategoryDropdown"
-                                class="text-blue-500 px-1 py-1 rounded-md border-blue-500 hover:bg-blue-500 hover:text-white">
-                                <i class="fa-solid fa-pen"></i>
-                            </button>
-                        </Transition>
-                        <Transition>
-                            <button v-show="selectedCategory.id" data-dropdown-toggle="dropdownDeleteCategory"
-                                class="text-red-500 px-1 py-1 rounded-md border-red-500 hover:bg-red-500 hover:text-white">
-                                <i class="fa-solid fa-trash"></i>
-                            </button>
-                        </Transition>
-                        <button data-dropdown-toggle="dropdownAddCategory" type="button" id="toggleAddCategoryDropdown"
-                            class="text-green-500 px-1 py-1 rounded-md hover:bg-green-500 hover:text-white">
-                            <i class="fa-solid fa-plus"></i>
-                        </button>
-                    </div>
-                    <form id="dropdownAddCategory" @submit.prevent="addNewCategory"
-                        class="!m-0 flex flex-col hidden z-10 gap-4 rounded-md bg-white shadow-md p-4">
-                        <h5 class="heading-5">Kategorie hinzufügen</h5>
-                        <TextInput type="text" placeholder="Name" v-model="addCategoryForm.name" required />
-                        <TextInput type="text" placeholder="Farbe" v-model="addCategoryForm.color" required />
-                        <button type="submit"
-                            class="text-white px-2 py-1 w-full hover:text-green-500 hover:bg-white border-2 border-green-500 bg-green-500 rounded-md">
-                            <i class="fa-solid fa-save"></i>
-                        </button>
-                    </form>
+
+                    <CategoryControlls @reset-selectable-values="resetSelectableValues"
+                        :selected-category="selectedCategory"></CategoryControlls>
                 </div>
-                <form id="dropdownEditCategory" @submit.prevent="updateCategory"
-                    class="!m-0 flex flex-col z-10 gap-4 rounded-md bg-white shadow-md p-4 hidden">
-                    <h5 class="heading-5">Kategorie bearbeiten</h5>
-                    <TextInput type="text" placeholder="Name" v-model="selectedCategory.name" />
-                    <TextInput type="text" placeholder="Farbe" v-model="selectedCategory.color" />
-                    <button type="submit"
-                        class="text-white px-2 py-1 w-full hover:text-green-500 hover:bg-white border-2 border-green-500 bg-green-500 rounded-md">
-                        <i class="fa-solid fa-save"></i>
-                    </button>
-                </form>
-                <form id="dropdownDeleteCategory" @submit.prevent="deleteSelectedCategory"
-                    class="!m-0 flex flex-col hidden z-10 gap-4 rounded-md bg-white shadow-md p-4">
-                    <h5 class="heading-5">Kategorie Löschen</h5>
-                    <div class="text-center">
-                        <p>
-                            Soll die Kategorie <span class="font-bold">"{{ selectedCategory.name }}"</span>
-                        </p>
-                        <p>
-                            wirklich gelöscht werden?
-                        </p>
-                    </div>
-                    <button type="submit"
-                        class="text-white px-2 py-1 w-full hover:text-red-500 hover:bg-white border-2 border-red-500 bg-red-500 rounded-md">
-                        <i class="fa-solid fa-trash"></i>
-                    </button>
-                </form>
                 <ul class="space-y-2">
                     <button v-for="category in categories" class="w-full text-start" @click="toggleEditCategory(category)">
                         <li class="category border-gray-400 text-gray-400"
@@ -120,12 +68,10 @@
 </template>
 
 <script setup>
-import BaseModal from '@/Components/BaseModal.vue';
-import { Transition, TransitionGroup, ref, onMounted } from 'vue';
-import TextInput from '@/Components/TextInput.vue';
+import BaseModal from '@/Components/BaseModal.vue'
+import { Transition, TransitionGroup, ref, onMounted } from 'vue'
+import CategoryControlls from '@/Pages/Suppliers/Partials/CategoryControlls.vue'
 import { initDropdowns } from 'flowbite'
-import { useForm } from '@inertiajs/inertia-vue3';
-import { Inertia } from '@inertiajs/inertia';
 
 onMounted(() => {
     initDropdowns()
@@ -147,34 +93,10 @@ let categoryTags = ref([])
 let selectedCategory = ref({})
 let selectedTag = ref({})
 
-const addCategoryForm = useForm(
-    {
-        name: null,
-        color: null,
-    },
-)
-
-function addNewCategory() {
-    if (addCategoryForm.name && addCategoryForm.color) {
-        addCategoryForm.post(route('category.store'))
-        document.getElementById('toggleAddCategoryDropdown').click()
-        addCategoryForm.reset()
-    }
-}
-
 function resetSelectableValues() {
     selectedCategory.value = {}
     categoryTags.value = []
     selectedTag.value = {}
-}
-
-function deleteSelectedCategory() {
-    const idToDelete = selectedCategory.value.id
-
-    if (idToDelete) {
-        resetSelectableValues()
-        Inertia.delete(route('category.destroy', idToDelete))
-    }
 }
 
 function toggleEditCategory(category) {
@@ -188,13 +110,6 @@ function toggleEditCategory(category) {
     categoryTags.value = props.tags.filter(tag => {
         return category.id === tag.category_id
     })
-}
-
-function updateCategory() {
-    if (selectedCategory.value.name && selectedCategory.value.color) {
-        Inertia.patch(route('category.patch', selectedCategory.value))
-        document.getElementById('toggleEditCategoryDropdown').click()
-    }
 }
 
 function toggleEditTag(tag) {
