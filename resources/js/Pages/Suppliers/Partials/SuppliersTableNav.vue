44761c100d834b0b4c0f4164375d6a53de59fbcd
bp <bartek.paraf@gmail.com>
2023-04-20 06:52:56 +0200

    changed styling to match the app

--- resources/js/Pages/Suppliers/Partials/SuppliersTableNav.vue
+++ resources/js/Pages/Suppliers/Partials/SuppliersTableNav.vue
@@ -11,9 +11,8 @@
                             clip-rule="evenodd"></path>
                     </svg>
                 </div>
-                <input type="text" id="table-search" v-model="searchInput" @keyup="searchForTerm"
-                    class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Suche">
+                <input type="text" id="table-search" v-model="searchInput" @keyup="searchForTerm" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50
+                            focus:ring-purchaser-primary focus:border-purchaser-primary" placeholder="Suche">
             </div>
             <div class="h-full space-x-3">
                 <BaseButton :href="route('suppliers.create')" color="primary">
