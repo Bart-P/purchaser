086c30bdc9fd9ea47be4ebdc12268dfa2dd53648
bp <bartek.paraf@gmail.com>
2023-04-20 07:11:28 +0200

    table nav is now in one line not 2

--- resources/js/Pages/Suppliers/Partials/SuppliersTableNav.vue
+++ resources/js/Pages/Suppliers/Partials/SuppliersTableNav.vue
@@ -1,18 +1,53 @@
 <template>
-    <div class="px-6 py-4 bg-white dark:bg-gray-900 w-full space-y-4">
+    <div class="px-6 pb-6 bg-white dark:bg-gray-900 w-full space-y-4">
         <div class=" flex justify-between">
-            <label for="table-search" class="sr-only">Search</label>
-            <div class="relative mt-1">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor"
-                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                            clip-rule="evenodd"></path>
-                    </svg>
+            <div class="flex align-middle justify-center items-center gap-3">
+                <div class="">
+                    <label for="table-search" class="sr-only">Search</label>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                        </div>
+                        <input type="text" id="table-search" v-model="searchInput" @keyup="searchForTerm"
+                            class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-md w-80 bg-gray-50
+                                                                                                                            focus:ring-purchaser-primary focus:border-purchaser-primary"
+                            placeholder="Suche">
+                    </div>
                 </div>
-                <input type="text" id="table-search" v-model="searchInput" @keyup="searchForTerm" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50
-                            focus:ring-purchaser-primary focus:border-purchaser-primary" placeholder="Suche">
+                <div class="space-x-3">
+                    <SelectItemDropdown id="categoryDropdown" @toggle-check-item="toggleCheckCategory" color="light"
+                        :items="categories" :checked-items="selectedCategory">
+                        Kategorie filter <i class="ml-2 fa-solid fa-angle-down"></i>
+                        <transition>
+                            <div v-show="selectedCategory.length"
+                                class="inline-flex items-center align-middle justify-center ml-2 text-sm bg-purchaser-secondary w-6 h-6 rounded-full text-white font-bold -right-4">
+                                {{ selectedCategory.length }}
+                            </div>
+                        </transition>
+                    </SelectItemDropdown>
+
+                    <SelectItemDropdown id="tagDropdown" @toggle-check-item="toggleCheckTag"
+                        :is-disabled="categoryTags.length < 1" color="light" :items="categoryTags"
+                        :checked-items="filterByTags">
+                        Tag filter <i class="ml-2 fa-solid fa-angle-down"></i>
+                        <transition>
+                            <div v-show="filterByTags.length"
+                                class="inline-flex items-center justify-center ml-2 text-sm bg-purchaser-secondary w-6 h-6 rounded-full text-white font-bold -right-4">
+                                {{ filterByTags.length }}
+                            </div>
+                        </transition>
+                    </SelectItemDropdown>
+
+                    <BaseButton @click="resetFields" color="secondary" class="rounded-full !px-3"
+                        :disabled="filterByTags.length + categoryTags.length + searchInput.length < 1">
+                        <i class="fa-solid fa-filter-circle-xmark"></i>
+                    </BaseButton>
+                </div>
             </div>
             <div class="h-full space-x-3">
                 <BaseButton :href="route('suppliers.create')" color="primary">
@@ -22,33 +57,6 @@
                     Zur Anfrage Hinzufügen
                 </BaseButton>
             </div>
-        </div>
-        <div class="space-x-3">
-            <SelectItemDropdown id="categoryDropdown" @toggle-check-item="toggleCheckCategory" color="light"
-                :items="categories" :checked-items="selectedCategory">
-                Kategorie filter
-                <transition>
-                    <div v-show="selectedCategory.length"
-                        class="inline-flex items-center align-middle justify-center ml-2 text-sm bg-purchaser-secondary w-6 h-6 rounded-full text-white font-bold -right-4">
-                        {{ selectedCategory.length }}
-                    </div>
-                </transition>
-            </SelectItemDropdown>
-
-            <SelectItemDropdown id="tagDropdown" @toggle-check-item="toggleCheckTag" :is-disabled="categoryTags.length < 1"
-                color="light" :items="categoryTags" :checked-items="filterByTags">
-                Tag filter
-                <transition>
-                    <div v-show="filterByTags.length"
-                        class="inline-flex items-center justify-center ml-2 text-sm bg-purchaser-secondary w-6 h-6 rounded-full text-white font-bold -right-4">
-                        {{ filterByTags.length }}
-                    </div>
-                </transition>
-            </SelectItemDropdown>
-
-            <BaseButton @click="resetFields" color="secondary"
-                :disabled="filterByTags.length + categoryTags.length + searchInput.length < 1">
-                Reset</BaseButton>
         </div>
     </div>
 </template>
