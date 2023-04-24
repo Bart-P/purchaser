0d35a30020a8559a72415019f8a594db554c376f
bp <bartek.paraf@gmail.com>
2023-04-24 07:06:38 +0200

    created IconButton component

---
+++ resources/js/Components/IconButton.vue
@@ -0,0 +1,51 @@
+<template>
+    <component :is="this.$attrs.href ? Link : 'button'" :class="this.$attrs.disabled ? buttonStyles.disabled : getClass()">
+        <slot />
+    </component>
+</template>
+
+<script setup>
+
+import { Link } from "@inertiajs/inertia-vue3";
+
+const props = defineProps(
+    {
+        color: {
+            type: String,
+            default: 'blue'
+        },
+    })
+
+const buttonStyles = {
+    blue: {
+        'cursor-pointer text-blue-600 hover:text-blue-800': true
+    },
+    red: {
+        'cursor-pointer text-red-600 hover:text-red-800': true
+    },
+    green: {
+        'cursor-pointer text-green-600 hover:text-green-800': true
+    },
+    gray: {
+        'cursor-pointer text-gray-600 hover:text-gray-800': true
+    },
+    disabled: {
+        'cursor-not-allowed text-gray-400': true
+    },
+}
+
+function getClass() {
+    switch (props.color) {
+        case 'blue':
+            return buttonStyles.blue
+        case 'red':
+            return buttonStyles.red
+        case 'green':
+            return buttonStyles.green
+        case 'gray':
+            return buttonStyles.gray
+        default:
+            return buttonStyles.blue
+    }
+}
+</script>
