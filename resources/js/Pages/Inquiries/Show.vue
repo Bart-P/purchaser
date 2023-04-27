2d43649d0f925b487ad92a093bbe63b8d11b9170
bp <bartek.paraf@gmail.com>
2023-04-27 20:50:21 +0200

    started inquiry show page

--- resources/js/Pages/Inquiries/Show.vue
+++ resources/js/Pages/Inquiries/Show.vue
@@ -8,7 +8,31 @@
 
         <div class="py-12">
             <PageBoxWrapper>
-                <h1 class="heading-1">{{ props.inquiry.title }}</h1>
+                <h1 class="heading-3 text-center">{{ props.inquiry.title }}</h1>
+                <table class="text-left">
+                    <tr class="table-row">
+                        <th class="table-data">Projekt Nr:</th>
+                        <td class="table-data">{{ props.inquiry.project }}</td>
+                    </tr>
+                    <tr class="table-row">
+                        <th class="table-data">Projekt Manager:</th>
+                        <td class="table-data">{{ props.inquiry.pm }}</td>
+                    </tr>
+                    <tr class="table-row">
+                        <th class="table-data">Kunde</th>
+                        <td class="table-data">{{ props.inquiry.client }}</td>
+                    </tr>
+                    <tr class="table-row">
+                        <th class="table-data">Erstellt am</th>
+                        <td class="table-data">{{ new Date(props.inquiry.created_at).toLocaleString('de').slice(0, -3) }}
+                        </td>
+                    </tr>
+                    <tr class="table-row">
+                        <th class="table-data">Letztes Update</th>
+                        <td class="table-data">{{ new Date(props.inquiry.updated_at).toLocaleString('de').slice(0, -3) }}
+                        </td>
+                    </tr>
+                </table>
             </PageBoxWrapper>
         </div>
     </AuthenticatedLayout>
@@ -17,7 +41,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import PageBoxWrapper from '@/Components/PageBoxWrapper.vue'
-import Head from '@inertiajs/inertia-vue3'
+import { Head } from "@inertiajs/inertia-vue3";
 
 const props = defineProps({
     inquiry: Object
