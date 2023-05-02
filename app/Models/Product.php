124033d854b2dd1de3f678d3a008f59af4e062e7
bp <bartek.paraf@gmail.com>
2023-05-02 07:10:25 +0200

    product model created

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'cost',
+        'price',
+    ];
+
+    public function inquiries(): BelongsTo
+    {
+        return $this->belongsTo(Inquiry::class);
+    }
+}
