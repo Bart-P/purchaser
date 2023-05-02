ed5d0603772083456f654e6f176e14d8f87cd4ec
bp <bartek.paraf@gmail.com>
2023-05-02 07:16:32 +0200

    product-price model created

---
+++ app/Models/ProductPrice.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ProductPrice extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'amount',
+        'cost_in_cent',
+        'price_in_cent',
+    ];
+
+    public function inquiries(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
