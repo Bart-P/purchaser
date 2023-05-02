1f42951e0ff367950aec68371946a560bf31e01b
bp <bartek.paraf@gmail.com>
2023-05-02 07:24:42 +0200

    product-price migration created

---
+++ database/migrations/2023_05_02_071754_create_product_prices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_prices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id');
+            $table->integer('quantity');
+            $table->integer('cost_in_cent');
+            $table->integer('price_in_cent');
+            $table->text('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_prices');
+    }
+};
