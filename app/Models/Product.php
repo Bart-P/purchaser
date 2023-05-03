045c0645cb3d4ed4d40691c434df7a3562a88c97
bp <bartek.paraf@gmail.com>
2023-05-03 07:39:50 +0200

    added price to product relation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -19,5 +20,10 @@ class Product extends Model
     public function inquiries(): BelongsTo
     {
         return $this->belongsTo(Inquiry::class);
+    }
+
+    public function productPrices(): HasMany
+    {
+        return $this->hasMany(ProductPrice::class);
     }
 }
