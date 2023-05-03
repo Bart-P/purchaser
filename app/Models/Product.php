429639010e83e023a8ae2ddb2c5a23a4adbd5118
bp <bartek.paraf@gmail.com>
2023-05-03 20:16:46 +0200

    removed, and added a new todo comment

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,9 +11,7 @@ class Product extends Model
 {
     use HasFactory;
 
-    // TODO what to do about description? 
-    // 1. either 3 in DB for each Product 
-    // 2. or separate table, each Product can have 5 Descriptions if needed
+    // TODO description is now a separate db. 
 
     protected $fillable = [
         'title',
