588089b75267443ef0f9830763ead313e00f019c
bp <bartek.paraf@gmail.com>
2023-05-07 10:28:44 +0200

    starten edit inquiry form

--- resources/js/Pages/Inquiries/Edit.vue
+++ resources/js/Pages/Inquiries/Edit.vue
@@ -3,10 +3,7 @@
     <AuthenticatedLayout>
         <template #header>
             <div class="flex justify-between">
-                <div class="">
-                    Anfrage ID {{ props.inquiry.id }}:
-                    <span class="text-gray-500">{{ props.inquiry.title }}</span>
-                </div>
+                <div class="">Anfrage (ID {{ inquiryDataForm.id }}) Daten Bearbeiten:</div>
                 <div class="space-x-3">
                     <BaseButton color="danger" class="!px-3 rounded-full text-sm">
                         <i class="fa-solid fa-trash"></i>
@@ -19,7 +16,31 @@
         </template>
 
         <div class="py-12">
-            <PageBoxWrapper>test</PageBoxWrapper>
+            <PageBoxWrapper>
+                <div class="flex gap-3 w-full">
+                    <form class="w-1/2" @submit.prevent="" action="#">
+                        <div>
+                            <InputLabel for="title">Titel:</InputLabel>
+                            <TextInput id="title" v-model="inquiryDataForm.title" type="text" name="title"></TextInput>
+                        </div>
+                        <div>
+                            <InputLabel for="status">Status:</InputLabel>
+                            <TextInput id="status" v-model="inquiryDataForm.status" type="text" name="title"></TextInput>
+                        </div>
+                    </form>
+                    <form class="w-1/2">
+                        <div class="">
+                            <!-- TODO - style textarea, new component?  -->
+                            <InputLabel for="description">Beschreibung</InputLabel>
+                            <TextInput id="description" v-model="inquiryDataForm.description" type="textarea"
+                                name="description">
+                            </TextInput>
+                        </div>
+                    </form>
+
+
+                </div>
+            </PageBoxWrapper>
         </div>
     </AuthenticatedLayout>
 </template>
@@ -28,11 +49,14 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import PageBoxWrapper from '@/Components/PageBoxWrapper.vue'
 import BaseButton from '@/Components/BaseButton.vue'
-import { Head } from "@inertiajs/vue3";
+import InputLabel from '@/Components/InputLabel.vue'
+import TextInput from '@/Components/TextInput.vue'
+import { Head, useForm } from "@inertiajs/vue3";
 
 const props = defineProps({
     inquiry: Object,
 })
 
-</script>
+const inquiryDataForm = useForm(props.inquiry)
 
+</script>
