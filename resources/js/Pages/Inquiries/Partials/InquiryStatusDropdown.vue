1d3c03e7ed70d010b5b6a03cb21dd73df253e8a0
bp <bartek.paraf@gmail.com>
2023-05-18 20:21:03 +0200

    inquiryStatusDropdown updates now db when needed
    when a inquiry is passed as a prop, it will be directly updated in db on click. If there is no inquiry passed there is just an emit

--- resources/js/Pages/Inquiries/Partials/InquiryStatusDropdown.vue
+++ resources/js/Pages/Inquiries/Partials/InquiryStatusDropdown.vue
@@ -1,13 +1,15 @@
 <template>
     <!-- Toggle status Dropdown button -->
-    <button id="selectStatusDropdownBtn" data-dropdown-toggle="selectStatusDropdown" type="button" class="rounded-md">
-        <StatusBadge :status="currentStatus" />
-        <i class="fa-solid fa-angle-down"></i>
+    <button :id="btnId" :data-dropdown-toggle="id" type="button" class="rounded-md">
+        <div class="flex flex-nowrap items-center">
+            <StatusBadge :status="currentStatus" />
+            <i class="fa-solid fa-angle-down"></i>
+        </div>
     </button>
 
     <!-- Dropdown menu -->
-    <div id="selectStatusDropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow px-2 py-1">
-        <ul class="py-2 text-sm text-gray-700 space-y-3" aria-labelledby="selectStatusDropdownBtn">
+    <div :id="id" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow px-2 py-1">
+        <ul class="py-2 text-sm text-gray-700 space-y-3" :aria-labelledby="btnId">
             <li v-for="status in statusKeys">
                 <button @click="selectStatus(status)" type="button" class="w-full align-middle">
                     <StatusBadge :status="status" />
@@ -22,6 +24,7 @@ import StatusBadge from '@/Components/StatusBadge.vue'
 import { allStatus, } from '@/utils'
 import { onMounted } from 'vue'
 import { initDropdowns } from 'flowbite'
+import { router } from '@inertiajs/core';
 
 onMounted(() => {
     initDropdowns()
@@ -30,13 +33,30 @@ onMounted(() => {
 const emits = defineEmits(['selectStatus']);
 
 const props = defineProps({
+    id: {
+        type: String,
+        default: 'selectStatusDropdown'
+    },
     currentStatus: String,
+    autoUpdateInquiry: {
+        type: Object,
+        defaulte: null
+    }
 })
+
+const btnId = props.id + '-btn'
 
 const statusKeys = Object.keys(allStatus.de)
 
 function selectStatus(status) {
+    if (props.autoUpdateInquiry) {
+        let newInquiry = { ...props.autoUpdateInquiry }
+        newInquiry.status = status
+        router.patch(route('inquiries.patch', newInquiry))
+    }
+
     emits('selectStatus', status)
-    document.getElementById('selectStatusDropdownBtn').click()
+    document.getElementById(btnId).click()
 }
 </script>
+
