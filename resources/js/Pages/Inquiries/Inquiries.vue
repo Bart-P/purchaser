4b7cb1452b6ffb6df3b8a9d438196c57ac54667f
bp <bartek.paraf@gmail.com>
2023-05-20 19:51:16 +0200

    link to create inquiry form

--- resources/js/Pages/Inquiries/Inquiries.vue
+++ resources/js/Pages/Inquiries/Inquiries.vue
@@ -8,6 +8,12 @@
 
         <div class="py-12">
             <PageBoxWrapper>
+
+                <div class="w-full">
+                    <BaseButton :href="route('inquiries.create')" color="success" btn-type="rounded" class="ml-auto mb-3">
+                        <i class="fa-solid fa-plus"></i>
+                    </BaseButton>
+                </div>
                 <InquiriesTable :inquiries="inquiries"></InquiriesTable>
             </PageBoxWrapper>
         </div>
@@ -20,6 +26,7 @@ import PageBoxWrapper from "@/Components/PageBoxWrapper.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
 import InquiriesTable from "@/Components/InquiriesTable.vue"
+import BaseButton from "@/Components/BaseButton.vue";
 
 const props = defineProps({
     inquiries: Object
