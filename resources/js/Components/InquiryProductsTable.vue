137a49ad932f539a72eef793fc44943b014cf8e2
bp <bartek.paraf@gmail.com>
2023-05-24 18:43:34 +0200

    product form modal used

--- resources/js/Components/InquiryProductsTable.vue
+++ resources/js/Components/InquiryProductsTable.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="flex justify-between mb-3">
         <h3 class="heading-3 pb-3">Produkte</h3>
-        <BaseButton @click="" color="success" btn-type="rounded">
+        <BaseButton @click="showProductForm" color="success" btn-type="rounded">
             <i class="fa-solid fa-cart-plus"></i>
         </BaseButton>
     </div>
+    <ProductFormModal id='productFormModal' />
     <table class="table">
         <thead class="table-head">
             <tr>
@@ -49,13 +50,30 @@
 <script setup>
 import BaseButton from '@/Components/BaseButton.vue';
 import IconButton from '@/Components/IconButton.vue';
+import ProductFormModal from '@/Components/Products/ProductFormModal.vue';
 import { dateToDMYHM } from '@/utils';
+import { initModals } from 'flowbite';
+import { onMounted } from 'vue';
+
+let productFormModal
+
+onMounted(() => {
+    initModals()
+    productFormModal = new Modal(document.getElementById('productFormModal'))
+})
 
 const props = defineProps({
     products: Object,
 });
 
+let selectedProduct
+
 function sortPricesByQuantities($prices) {
     return $prices?.sort((a, b) => a.quantity - b.quantity);
+}
+
+function showProductForm() {
+    productFormModal.show()
+
 }
 </script>
