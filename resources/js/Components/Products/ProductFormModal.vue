58816a5e0faa82c0c981f813e9f7fd002e69c8d2
bp <bartek.paraf@gmail.com>
2023-05-28 20:56:23 +0200

    started tabbed layout for description

--- resources/js/Components/Products/ProductFormModal.vue
+++ resources/js/Components/Products/ProductFormModal.vue
@@ -16,17 +16,56 @@
                         autofocus />
                 </div>
 
-                <div v-if="productFormData.description" v-for="(desc, index) in productFormData.description">
-                    <InputLabel :for="'description' + index" :value="'Beschreibung ' + (index + 1)" />
-                    <div class="border border-gray-300 px-4 py-2 bg-white rounded-md focus:border-purchaser-primary">
-                        <TextArea :id="'description' + index" :key="'description-' + index" v-model="desc.description"
-                            required />
+                <div class="mb-4 border-b border-gray-200">
+                    <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="descriptionTab"
+                        data-tabs-toggle="#descriptionTabContent" role="tablist">
+                        <li v-for="desc in productFormData.description" class="mr-2" role="presentation">
+                            <button class="inline-block p-4 border-b-2 rounded-t-lg" :id="'description-tab-btn-' + desc.id"
+                                :data-tabs-target="'#description-id-' + desc.id" type="button" role="tab"
+                                :aria-controls="'description-control-id-' + desc.id" aria-selected="true">{{ desc.title }}
+                            </button>
+                        </li>
+                        <!-- <li class="mr-2" role="presentation"> -->
+                        <!--     <button -->
+                        <!--         class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300" -->
+                        <!--         id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" -->
+                        <!--         aria-controls="dashboard" aria-selected="false">Dashboard</button> -->
+                        <!-- </li> -->
+                        <!-- <li class="mr-2" role="presentation"> -->
+                        <!--     <button -->
+                        <!--         class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300" -->
+                        <!--         id="settings-tab" data-tabs-target="#settings" type="button" role="tab" -->
+                        <!--         aria-controls="settings" aria-selected="false">Settings</button> -->
+                        <!-- </li> -->
+                        <!-- <li role="presentation"> -->
+                        <!--     <button -->
+                        <!--         class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300" -->
+                        <!--         id="contacts-tab" data-tabs-target="#contacts" type="button" role="tab" -->
+                        <!--         aria-controls="contacts" aria-selected="false">Contacts</button> -->
+                        <!-- </li> -->
+                    </ul>
+                </div>
+                <div id="descriptionTabContent">
+                    <div v-if="productFormData.description" v-for="(desc, index) in productFormData.description"
+                        :id="'description-id-' + desc.id" role="tabpanel" :aria-labelledby="'description-tab-btn-' +
+                            desc.id" class="hidden">
+                        <InputLabel :for="'description' + index" :value="'Beschreibung ' + (index + 1)" />
+                        <div class="border border-gray-300 px-4 py-2 bg-white rounded-md focus:border-purchaser-primary">
+                            <TextArea :id="'description' + index" :key="'description-' + index" v-model="desc.description"
+                                required />
+                        </div>
                     </div>
+                    <!-- <div class="hidden p-4 rounded-lg bg-gray-50" :id="'description-control-id-' + desc.id" role="tabpanel" -->
+                    <!--     :aria-labelledby="'description-tab-btn-' + desc.id"> -->
+                    <!--     <p class="text-sm text-gray-500">This is some placeholder content the <strong -->
+                    <!--             class="font-medium text-gray-800">Profile tab's associated content</strong>. -->
+                    <!--         Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript -->
+                    <!--         swaps classes to control the content visibility and styling.</p> -->
+                    <!-- </div> -->
                 </div>
 
                 <!-- Modal footer -->
-                <div
-                    class="flex items-center justify-end p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <div class="flex items-center justify-end p-6 space-x-2 border-t border-gray-200 rounded-b">
                     <BaseButton color="primary" @click.prevent="" type="submit">
                         Speichern
                     </BaseButton>
