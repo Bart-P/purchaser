26a0fddb7c6f5700021b556fbf85736b09f430ea
bp <bartek.paraf@gmail.com>
2023-05-28 19:56:36 +0200

    Standalone TextArea component created

---
+++ resources/js/Components/TextArea.vue
@@ -0,0 +1,23 @@
+<template>
+    <textarea class="w-full px-0 text-gray-900 bg-white border-0 focus:ring-0" :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)" ref="input" />
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+defineProps(['modelValue']);
+
+defineEmits(['update:modelValue']);
+
+const input = ref(null);
+
+onMounted(() => {
+    if (input.value.hasAttribute('autofocus')) {
+        input.value.focus();
+    }
+});
+
+defineExpose({ focus: () => input.value.focus() });
+</script>
+
