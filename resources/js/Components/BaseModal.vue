727e2d59c8cd3b54487ea784838f8b6d5efdd689
bp <bartek.paraf@gmail.com>
2023-07-01 11:27:58 +0200

    added y-overflow scroll so there is nothing cut off

--- resources/js/Components/BaseModal.vue
+++ resources/js/Components/BaseModal.vue
@@ -1,8 +1,9 @@
 <template>
     <Teleport to="body">
         <div :id="id" tabindex="-1" aria-hidden="true"
-            class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-            <div class="relative w-full h-full md:h-auto bg-white rounded-lg shadow" :class="'max-w-' + width">
+            class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-hidden md:inset-0 h-modal md:h-full">
+            <div class="relative w-full md:h-auto max-h-screen bg-white rounded-lg shadow md:overflow-y-scroll"
+                :class="'max-w-' + width">
                 <slot />
             </div>
         </div>
